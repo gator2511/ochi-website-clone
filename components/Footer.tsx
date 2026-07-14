959e944262f303eb250f689a7731fd8d2a49c90c
gator2511 <gundeepthethy25@hotmail.com>
2026-07-14 17:42:53 +0930

    Update footer contact details and branding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,9 @@ import { footerItems, footernavbarItems } from "@/constants";
 export default function Footer() {
 	const phrase = ["Eye-", "opening"];
 	const phrase1 = ["PRESENTATIONS"];
+	const locationHref =
+		"https://www.google.com/maps/search/?api=1&query=130+Smith+Street+Darwin+City+NT+0800";
+
 	return (
 		<footer className="w-full min-h-screen padding-x z-30 relative pt-[40px] bg-background flex flex-col justify-between rounded-t-[20px] mt-[-20px]">
 			<div className="w-full flex justify-between sm:flex-col xm:flex-col">
@@ -40,24 +43,19 @@ export default function Footer() {
 								</h1>
 								<div className="flex flex-col gap-y-[10px]">
 									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title={`202-1965 W 4th Awe`}
-										href="/"
+										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+										title="130 Smith Street, Darwin City"
+										href={locationHref}
 									/>
 									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title={`Vancouver, Canada`}
-										href="/"
+										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+										title="Darwin, Northern Territory"
+										href={locationHref}
 									/>
 									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title="30 Chukarina"
-										href="/"
-									/>
-									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title="St Lviv, Ukraine"
-										href="/"
+										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+										title="0800"
+										href={locationHref}
 									/>
 								</div>
 							</div>
@@ -80,8 +78,8 @@ export default function Footer() {
 								E:
 							</h1>
 							<LinkHover
-								title="hello@ochi.design"
-								href="/"
+								title="gundeep@gtmarketing.io"
+								href="mailto:gundeep@gtmarketing.io"
 								className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
 							/>
 						</div>
@@ -90,19 +88,20 @@ export default function Footer() {
 			</div>
 			<div className="w-full pt-[40px] pb-[30px] flex justify-between sm:flex-col xm:flex-col sm:gap-[20px] xm:gap-[20px]">
 				<div className="w-1/2 sm:w-full xm:w-full">
-					<Link href={"/"}>
+					<Link href="/" aria-label="GT Marketing home">
 						<Image
 							src={logo}
-							alt="ochi logo"
-							width={70}
-							height={70}
+							alt="GT Marketing logo"
+							width={38}
+							height={56}
+							className="h-[52px] w-auto object-contain"
 						/>
 					</Link>
 				</div>
 				<div className="w-1/2 h-full flex gap-[10px] justify-between items-end sm:w-full xm:w-full sm:flex-col xm:flex-col sm:items-start xm:items-start">
 					<div className="flex sm:flex-col xm:flex-col gap-[10px]">
 						<h1 className="paragraph font-medium font-NeueMontreal text-secondry opacity-40">
-							© ochi design 2024.
+							© GT Studio 2026
 						</h1>
 						<LinkHover
 							title="Legal Terms"
@@ -112,8 +111,8 @@ export default function Footer() {
 					</div>
 					<div>
 						<LinkHover
-							title="Website by Zain"
-							href="/"
+							title="Website by GT Marketing"
+							href="https://gtmarketing.io"
 							className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
 						/>
 					</div>
