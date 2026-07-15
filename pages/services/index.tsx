4ccd3c5f048666649a20274d305f132fcb1744f7
gator2511 <gundeepthethy25@hotmail.com>
2026-07-15 10:23:57 +0930

    Place immersive brand imagery on services page

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -8,21 +8,23 @@ import {
 	Heroservices,
 } from "@/container";
 import { useEffect } from "react";
-import { Curve, Ready } from "@/components";
+import { BrandImageGallery, Curve, Ready } from "@/components";
 
 export default function Services() {
 	useEffect(() => {
 		(async () => {
 			const LocomotiveScroll = (await import("locomotive-scroll")).default;
-			const locomotiveScroll = new LocomotiveScroll();
+			new LocomotiveScroll();
 		})();
 	}, []);
+
 	return (
 		<>
-			<Curve backgroundColor={"#f1f1f1"}>
+			<Curve backgroundColor="#f1f1f1">
 				<Heroservices />
 				<Process />
 				<Capibilyties />
+				<BrandImageGallery variant="services" />
 				<Clientsservices />
 				<Archive />
 				<Expectations />
