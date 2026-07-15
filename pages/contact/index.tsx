217213bbdfbac1d1653ea1b8f01427ee2f80ab28
gator2511 <gundeepthethy25@hotmail.com>
2026-07-15 10:24:48 +0930

    Place immersive brand imagery on contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,20 +1,22 @@
 "use client";
 import { useEffect } from "react";
-import { Curve } from "@/components";
+import { BrandImageGallery, Curve } from "@/components";
 import { Herocontact, Form, FAQ, Socials } from "@/container";
 
 export default function Contact() {
 	useEffect(() => {
 		(async () => {
 			const LocomotiveScroll = (await import("locomotive-scroll")).default;
-			const locomotiveScroll = new LocomotiveScroll();
+			new LocomotiveScroll();
 		})();
 	}, []);
+
 	return (
 		<>
-			<Curve backgroundColor={"#f1f1f1"}>
+			<Curve backgroundColor="#f1f1f1">
 				<Herocontact />
 				<Form />
+				<BrandImageGallery variant="contact" />
 				<Socials />
 				<FAQ />
 			</Curve>
