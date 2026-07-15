8901f8d368f1e2a01ffc85cdcf68b55dd25e121d
gator2511 <gundeepthethy25@hotmail.com>
2026-07-15 10:24:37 +0930

    Place immersive brand imagery on insights page

--- pages/insights/index.tsx
+++ pages/insights/index.tsx
@@ -1,19 +1,21 @@
 "use client";
 import { useEffect } from "react";
-import { Curve } from "@/components";
+import { BrandImageGallery, Curve } from "@/components";
 import { Heroinsights, Publicationinsights } from "@/container";
 
 export default function Insights() {
 	useEffect(() => {
 		(async () => {
 			const LocomotiveScroll = (await import("locomotive-scroll")).default;
-			const locomotiveScroll = new LocomotiveScroll();
+			new LocomotiveScroll();
 		})();
 	}, []);
+
 	return (
 		<>
-			<Curve backgroundColor={"#f1f1f1"}>
+			<Curve backgroundColor="#f1f1f1">
 				<Heroinsights />
+				<BrandImageGallery variant="insights" />
 				<Publicationinsights />
 			</Curve>
 		</>
