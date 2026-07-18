7a3ae2edc31dc1002a44507a585cda2bb7904fcb
gator2511 <gundeepthethy25@hotmail.com>
2026-07-18 14:42:27 +0930

    Render Work page from Visual Editor content

--- pages/presentation/index.tsx
+++ pages/presentation/index.tsx
@@ -1,11 +1,12 @@
 "use client";
+
+import Head from "next/head";
 import { useEffect } from "react";
 import { BrandImageGallery, Curve, Ready } from "@/components";
-import {
-	Heropresentation,
-	Projectspresentation,
-	Publication,
-} from "@/container";
+import { Heropresentation, Projectspresentation, Publication } from "@/container";
+import content from "@/content/pages/work.json";
+
+const documentId = "content/pages/work.json";
 
 export default function Presentation() {
 	useEffect(() => {
@@ -17,13 +18,19 @@ export default function Presentation() {
 
 	return (
 		<>
-			<Curve backgroundColor="#f1f1f1">
-				<Heropresentation />
-				<Projectspresentation />
-				<BrandImageGallery variant="work" />
-				<Publication />
-				<Ready />
-			</Curve>
+			<Head>
+				<title>{content.seoTitle}</title>
+				<meta name="description" content={content.seoDescription} />
+			</Head>
+			<div data-sb-object-id={documentId}>
+				<Curve backgroundColor="#f1f1f1">
+					<Heropresentation content={content.hero} />
+					<Projectspresentation intro={content.intro} projects={content.projects} />
+					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+					<Publication content={content.publication} />
+					<Ready />
+				</Curve>
+			</div>
 		</>
 	);
 }
