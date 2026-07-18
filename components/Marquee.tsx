4f93561a47dfa176e8d8d06bfcf94d417c987dee
gator2511 <gundeepthethy25@hotmail.com>
2026-07-18 14:27:38 +0930

    Annotate marquee text for inline editing

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,15 +1,17 @@
 import { TMarqueeProps } from "@/types";
 import { TextMarquee } from "@/animation";
 
-export default function Marquee({ title, className }: TMarqueeProps) {
+export default function Marquee({ title, className, fieldPath }: TMarqueeProps) {
 	return (
 		<TextMarquee baseVelocity="0.7">
 			<h1
-				className={`font-FoundersGrotesk bg-marquee font-normal border-y border-[#ffffff55] uppercase text-white  whitespace-nowrap tracking-[-5px] ${className}`}>
+				data-sb-field-path={fieldPath}
+				className={`font-FoundersGrotesk bg-marquee font-normal border-y border-[#ffffff55] uppercase text-white whitespace-nowrap tracking-[-5px] ${className}`}>
 				{title} &nbsp;
 			</h1>
 			<h1
-				className={`font-FoundersGrotesk bg-marquee font-normal border-y border-[#ffffff55] uppercase text-white  whitespace-nowrap tracking-[-5px] ${className}`}>
+				aria-hidden="true"
+				className={`font-FoundersGrotesk bg-marquee font-normal border-y border-[#ffffff55] uppercase text-white whitespace-nowrap tracking-[-5px] ${className}`}>
 				{title} &nbsp;
 			</h1>
 		</TextMarquee>
