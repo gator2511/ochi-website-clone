5ceff5fe0766c5c5109157e734631266bee0b61d
gator2511 <gundeepthethy25@hotmail.com>
2026-07-18 14:52:42 +0930

    Render case study from Visual Editor content

--- pages/case/index.tsx
+++ pages/case/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import Head from "next/head";
 import {
 	Heroworkiz,
 	Aboutworkiz,
@@ -10,26 +12,36 @@ import {
 } from "@/container";
 import { useEffect } from "react";
 import { Curve, Ready } from "@/components";
+import content from "@/content/pages/case.json";
+
+const documentId = "content/pages/case.json";
 
 export default function Work() {
 	useEffect(() => {
 		(async () => {
 			const LocomotiveScroll = (await import("locomotive-scroll")).default;
-			const locomotiveScroll = new LocomotiveScroll();
+			new LocomotiveScroll();
 		})();
 	}, []);
+
 	return (
 		<>
-			<Curve backgroundColor="#f1f1f1">
-				<Heroworkiz />
-				<Aboutworkiz />
-				<Chelenge />
-				<VideoWorkiz />
-				<Result />
-				<Credit />
-				<Works />
-				<Ready />
-			</Curve>
+			<Head>
+				<title>{content.seoTitle}</title>
+				<meta name="description" content={content.seoDescription} />
+			</Head>
+			<div data-sb-object-id={documentId}>
+				<Curve backgroundColor="#f1f1f1">
+					<Heroworkiz content={content.hero} />
+					<Aboutworkiz content={content.company} />
+					<Chelenge content={content.challenge} />
+					<VideoWorkiz content={content.video} />
+					<Result content={content.result} />
+					<Credit content={content.credit} />
+					<Works content={content.related} />
+					<Ready />
+				</Curve>
+			</div>
 		</>
 	);
 }
