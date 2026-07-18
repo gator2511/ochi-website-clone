a50425b6f621e369ae17a60410af8148930fee0d
gator2511 <gundeepthethy25@hotmail.com>
2026-07-18 14:43:16 +0930

    Render Contact page from Visual Editor content

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,12 @@
 "use client";
+
+import Head from "next/head";
 import { useEffect } from "react";
 import { BrandImageGallery, Curve } from "@/components";
 import { Herocontact, Form, FAQ, Socials } from "@/container";
+import content from "@/content/pages/contact.json";
+
+const documentId = "content/pages/contact.json";
 
 export default function Contact() {
 	useEffect(() => {
@@ -13,13 +18,19 @@ export default function Contact() {
 
 	return (
 		<>
-			<Curve backgroundColor="#f1f1f1">
-				<Herocontact />
-				<Form />
-				<BrandImageGallery variant="contact" />
-				<Socials />
-				<FAQ />
-			</Curve>
+			<Head>
+				<title>{content.seoTitle}</title>
+				<meta name="description" content={content.seoDescription} />
+			</Head>
+			<div data-sb-object-id={documentId}>
+				<Curve backgroundColor="#f1f1f1">
+					<Herocontact content={content.hero} />
+					<Form content={content.form} />
+					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+					<Socials content={content.socials} />
+					<FAQ content={content.faq} />
+				</Curve>
+			</div>
 		</>
 	);
 }
