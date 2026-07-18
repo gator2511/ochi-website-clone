7ed6c795ae0e4f125b6ba8ca4780b605aea19c76
gator2511 <gundeepthethy25@hotmail.com>
2026-07-18 14:41:49 +0930

    Render homepage from Visual Editor content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import Head from "next/head";
 import { useEffect } from "react";
 import {
 	BrandImageGallery,
@@ -8,6 +10,9 @@ import {
 	Ready,
 } from "@/components";
 import { About, Clients, Hero, Projects, VideoHome } from "@/container";
+import content from "@/content/pages/home.json";
+
+const documentId = "content/pages/home.json";
 
 export default function Home() {
 	useEffect(() => {
@@ -19,22 +24,29 @@ export default function Home() {
 
 	return (
 		<>
-			<Curve backgroundColor="#f1f1f1">
-				<Hero />
-				<div className="w-full bg-marquee z-10 relative rounded-t-[20px] padding-y">
-					<Marquee
-						title="gt marketing"
-						className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[20px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
-					/>
-				</div>
-				<About />
-				<BrandImageGallery variant="home" />
-				<VideoHome />
-				<HomeVisionShowcase />
-				<Projects />
-				<Clients />
-				<Ready />
-			</Curve>
+			<Head>
+				<title>{content.seoTitle}</title>
+				<meta name="description" content={content.seoDescription} />
+			</Head>
+			<div data-sb-object-id={documentId}>
+				<Curve backgroundColor="#f1f1f1">
+					<Hero content={content.hero} />
+					<div className="w-full bg-marquee z-10 relative rounded-t-[20px] padding-y">
+						<Marquee
+							title={content.marqueeText}
+							fieldPath="marqueeText"
+							className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[20px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
+						/>
+					</div>
+					<About content={content.about} />
+					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+					<VideoHome content={content.video} />
+					<HomeVisionShowcase content={content.vision} fieldPath="vision" />
+					<Projects content={content.projects} />
+					<Clients content={content.reviews} />
+					<Ready />
+				</Curve>
+			</div>
 		</>
 	);
 }
