e64bf10e8d7f4c09a9914980b140e836738eac9d
gator2511 <gundeepthethy25@hotmail.com>
2026-07-23 11:11:01 +0930

    Add GT Marketing About Us route

---
+++ pages/about-us/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Head from "next/head";
+import { Heroabout } from "@/container";
+import { useEffect } from "react";
+import { BrandImageGallery, Curve, Ready } from "@/components";
+import content from "@/content/pages/about.json";
+
+const documentId = "content/pages/about.json";
+
+export default function About() {
+	useEffect(() => {
+		(async () => {
+			const LocomotiveScroll = (await import("locomotive-scroll")).default;
+			new LocomotiveScroll();
+		})();
+	}, []);
+
+	return (
+		<>
+			<Head>
+				<title>{content.seoTitle}</title>
+				<meta name="description" content={content.seoDescription} />
+				<meta property="og:title" content={content.seoTitle} />
+				<meta property="og:description" content={content.seoDescription} />
+				<meta property="og:url" content="https://gtmarketing.io/about-us" />
+			</Head>
+			<div data-sb-object-id={documentId}>
+				<Curve backgroundColor="#f1f1f1">
+					<Heroabout content={content.hero} />
+					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+					<Ready />
+				</Curve>
+			</div>
+		</>
+	);
+}
