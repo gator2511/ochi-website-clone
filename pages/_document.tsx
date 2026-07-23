0695e85092ef0417d7d0b1a64c6b6fae4753390b
gator2511 <gundeepthethy25@hotmail.com>
2026-07-23 11:08:55 +0930

    Remove legacy OCHI document title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <title>OCHI - Presentation Design Agency</title>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en-AU">
+			<Head>
+				<link rel="icon" href="/logo.svg" type="image/svg+xml" />
+				<link rel="shortcut icon" href="/logo.svg" />
+				<meta name="theme-color" content="#ff4b00" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
