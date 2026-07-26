f9dc14ddb9bd2417034393fdad27c9a6011719bb
gator2511 <gundeepthethy25@hotmail.com>
2026-07-26 10:47:37 +0930

    Remove duplicate Contact metadata

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import { useEffect } from "react";
 import { BrandImageGallery, Curve } from "@/components";
 import { Herocontact, Form, FAQ, Socials } from "@/container";
@@ -17,20 +16,14 @@ export default function Contact() {
 	}, []);
 
 	return (
-		<>
-			<Head>
-				<title>{content.seoTitle}</title>
-				<meta name="description" content={content.seoDescription} />
-			</Head>
-			<div data-sb-object-id={documentId}>
-				<Curve backgroundColor="#f1f1f1">
-					<Herocontact content={content.hero} />
-					<Form content={content.form} />
-					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
-					<Socials content={content.socials} />
-					<FAQ content={content.faq} />
-				</Curve>
-			</div>
-		</>
+		<div data-sb-object-id={documentId}>
+			<Curve backgroundColor="#f1f1f1">
+				<Herocontact content={content.hero} />
+				<Form content={content.form} />
+				<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+				<Socials content={content.socials} />
+				<FAQ content={content.faq} />
+			</Curve>
+		</div>
 	);
 }
