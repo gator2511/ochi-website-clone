3c6ee6ad6b6ab471e843b049785762d8a22b23f4
gator2511 <gundeepthethy25@hotmail.com>
2026-07-26 13:13:05 +0930

    Add local SEO Darwin and Palmerston blog page

---
+++ pages/blog/local-seo-checklist-darwin-palmerston-businesses.tsx
@@ -0,0 +1,8 @@
+import BlogArticleLayout from "@/components/BlogArticleLayout";
+import content from "@/content/pages/blog-local-seo-darwin-palmerston.json";
+
+const documentId = "content/pages/blog-local-seo-darwin-palmerston.json";
+
+export default function LocalSEODarwinPalmerstonArticle() {
+	return <BlogArticleLayout content={content} documentId={documentId} />;
+}
