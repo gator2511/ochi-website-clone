496d2331927b08055daf8eec4dbac990dedc89ac
gator2511 <gundeepthethy25@hotmail.com>
2026-07-26 10:46:49 +0930

    Remove duplicate Work metadata

--- pages/presentation/index.tsx
+++ pages/presentation/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import { useEffect } from "react";
 import { BrandImageGallery, Curve, Ready } from "@/components";
 import { Heropresentation, Projectspresentation, Publication } from "@/container";
@@ -17,20 +16,14 @@ export default function Presentation() {
 	}, []);
 
 	return (
-		<>
-			<Head>
-				<title>{content.seoTitle}</title>
-				<meta name="description" content={content.seoDescription} />
-			</Head>
-			<div data-sb-object-id={documentId}>
-				<Curve backgroundColor="#f1f1f1">
-					<Heropresentation content={content.hero} />
-					<Projectspresentation intro={content.intro} projects={content.projects} />
-					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
-					<Publication content={content.publication} />
-					<Ready />
-				</Curve>
-			</div>
-		</>
+		<div data-sb-object-id={documentId}>
+			<Curve backgroundColor="#f1f1f1">
+				<Heropresentation content={content.hero} />
+				<Projectspresentation intro={content.intro} projects={content.projects} />
+				<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+				<Publication content={content.publication} />
+				<Ready />
+			</Curve>
+		</div>
 	);
 }
