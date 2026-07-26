e1238e95a1e86111a7581dab72adf015d8ac7414
gator2511 <gundeepthethy25@hotmail.com>
2026-07-26 10:46:14 +0930

    Remove duplicate homepage metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import { useEffect } from "react";
 import {
 	BrandImageGallery,
@@ -23,30 +22,24 @@ export default function Home() {
 	}, []);
 
 	return (
-		<>
-			<Head>
-				<title>{content.seoTitle}</title>
-				<meta name="description" content={content.seoDescription} />
-			</Head>
-			<div data-sb-object-id={documentId}>
-				<Curve backgroundColor="#f1f1f1">
-					<Hero content={content.hero} />
-					<div className="w-full bg-marquee z-10 relative rounded-t-[20px] padding-y">
-						<Marquee
-							title={content.marqueeText}
-							fieldPath="marqueeText"
-							className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[20px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
-						/>
-					</div>
-					<About content={content.about} />
-					<BrandImageGallery content={content.gallery} fieldPath="gallery" />
-					<VideoHome content={content.video} />
-					<HomeVisionShowcase content={content.vision} fieldPath="vision" />
-					<Projects content={content.projects} />
-					<Clients content={content.reviews} />
-					<Ready />
-				</Curve>
-			</div>
-		</>
+		<div data-sb-object-id={documentId}>
+			<Curve backgroundColor="#f1f1f1">
+				<Hero content={content.hero} />
+				<div className="w-full bg-marquee z-10 relative rounded-t-[20px] padding-y">
+					<Marquee
+						title={content.marqueeText}
+						fieldPath="marqueeText"
+						className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[20px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
+					/>
+				</div>
+				<About content={content.about} />
+				<BrandImageGallery content={content.gallery} fieldPath="gallery" />
+				<VideoHome content={content.video} />
+				<HomeVisionShowcase content={content.vision} fieldPath="vision" />
+				<Projects content={content.projects} />
+				<Clients content={content.reviews} />
+				<Ready />
+			</Curve>
+		</div>
 	);
 }
