bd38f1924737c1f8bbbcb21469b0029e38b5e067
gator2511 <gundeepthethy25@hotmail.com>
2026-07-28 11:00:45 +0930

    Fix homepage build by removing nonexistent Cards import

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import {
 	Marquee,
 	Ready,
 } from "@/components";
-import { About, Cards, Clients, Hero, Projects, VideoHome } from "@/container";
+import { About, Clients, Hero, Projects, VideoHome } from "@/container";
 import content from "@/content/pages/home.json";
 
 const documentId = "content/pages/home.json";
@@ -36,7 +36,6 @@ export default function Home() {
 				<HomeVisionShowcase content={content.vision} fieldPath="vision" />
 				<Projects content={content.projects} />
 				<Clients content={content.reviews} />
-				<Cards />
 				<Ready />
 			</Curve>
 		</div>
